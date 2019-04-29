5150b2079a4902104e69071f45aaf99f929b81ff
Adeyemi Oyewusi <adeyemioyewusi@gmail.com>
2019-04-29 14:47:34 +0100

    models created

---
+++ Payroll.Models/Payroll.Models/Payroll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payroll.Models
+{
+    public class Payroll
+    {
+        public int Id { get; set; }
+        public int StaffId { get; set; }
+        public string Description { get; set; }
+        public string DeductionOrAddition { get; set; }
+        public string TaxCategory { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Payroll.Models/Payroll.Models/Staff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Payroll.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+   
+}
